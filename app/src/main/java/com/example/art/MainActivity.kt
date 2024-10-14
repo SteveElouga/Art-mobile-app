a0798a397d71d27b5d4ded3f635edcecf31192a5
SteveElouga <nyobeelouga5@gmail.com>
2024-10-14 21:01:54 +0100

    mise a jour de l'application, correctif et amelioration de l'experience utilisateur

--- app/src/main/java/com/example/art/MainActivity.kt
+++ app/src/main/java/com/example/art/MainActivity.kt
@@ -17,7 +17,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -182,8 +184,10 @@ fun ArtTextAndImage(
         modifier = modifier
             .padding(20.dp)
             .fillMaxSize()
+            .statusBarsPadding()
             .background(Color(0xFFF8F9FA))
             .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
     ) {
         Column(
             modifier = modifier
